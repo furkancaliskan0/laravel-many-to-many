277c5ec28537e21e7576bfeb91b9b1b87966b6c2
furkanGit <furkancaliskan9801@gmail.com>
2023-02-14 23:36:53 +0100

    created Home, Product, Create and Store routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Importo il "MainController".
 use App\Http\Controllers\MainController;
 
 
 
+// Home Route
+Route::get('/', [MainController::class, 'home'])
+    ->name('home');
 
-Route::get('/', [MainController::class, 'home']);
+// Product Route
+Route::get('/product', [MainController::class, 'products'])
+    ->name('product.home');
+
+// Create Route
+Route::get('/product/create', [MainController::class, 'productCreate'])
+    ->name('product.create');
+
+// Store Route
+Route::post('/product/create', [MainController::class, 'productStore'])
+    ->name('product.store');
